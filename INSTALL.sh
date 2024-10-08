240c5697933189dca44f7216f6814e5ac6ce675e
Jamie King <jamiedanielking@gmail.com>
2024-10-08 21:28:29 +0100

    Updated install script to reflect changes

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,22 +7,40 @@
 # --- device selection
 
 echo "What device are you installing on?"
+echo "01) GLaDOS"
+echo "02) Wheatley"
+echo "03) Caroline"
 echo "04) HX"
 echo ""
 read -p "Select a device: " DEVICE
 
 # --- install
 
+# kitty
+if [[ ${DEVICE} -eq 2 ]]; then
+
+    rm -r "${HOME}/.config/kitty"
+
+    mkdir -p "${HOME}/.config/kitty"
+
+    ln "./Kitty/kitty.conf" "${HOME}/.config/kitty/kitty.conf"
+
+fi
+
 # neovim
 rm -r "${HOME}/.config/nvim"
 
-mkdir "${HOME}/.config/nvim"
+mkdir -p "${HOME}/.config/nvim"
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-if [[ DEVICE -eq 4 ]]; then
+if [[ ${DEVICE} -eq 1 ]]; then
 
-    ln "./Neovim/hx-init.vim" "${HOME}/.config/nvim/init.vim"
+    ln "./Neovim/kde-init.vim" "${HOME}/.config/nvim/init.vim"
+
+elif [[ ${DEVICE} -eq 2 || ${DEVICE} -eq 3 || ${DEVICE} -eq 4 ]]; then
+
+    ln "./Neovim/cat-init.vim" "${HOME}/.config/nvim/init.vim"
 
 fi
 
@@ -37,5 +55,8 @@ rm "${HOME}/.zlogout"
 
 curl -L git.io/antigen > "${HOME}/.antigen.zsh"
 
-ln "./ZSH/zshenv" "${HOME}/.zshenv"
+if [[ ${DEVICE} -eq 4 ]]; then
+    ln "./ZSH/hx-zshenv" "${HOME}/.zshenv"
+fi
+
 ln "./ZSH/zshrc" "${HOME}/.zshrc"
